1022cfd8a82d1021c89b7fe0e2314bca53f4e682
VadymCodes <skoryivadym@gmail.com>
2023-04-19 08:51:21 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,52 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+    // production: false,
+
+    // HOST: 'http://localhost:4200',
+    // WEBCAL: 'webcal://localhost:5050',
+    // API_HOST: 'http://localhost:5050',
+    // REFERRAL_URL: "https://learnt.io",
+
+    // API_SOCKET_URL: 'ws://localhost:5050/ws',
+
+    // COOKIE_DOMAIN: 'localhost',
+
+    // MAPBOX_TOKEN: 'pk.eyJ1IjoidHV0b3JhcHAiLCJhIjoiY2luNjJrYThqMGU4NHZobTRrb3N5dW8ydyJ9.tWCvx9Qix0jRtOpYN695CQ',
+
+    // FacebookAppID: '434644221081502',
+
+    // GoogleMapsApiKey: '',
+
+    // INTERCOM_APP_ID: 'gv9udgbh',
+
+    // NATIVEFORMS_LICENSE: 'vj5hlyew',
+
+    // SURVEY_DAYS_RECURRENT: 7,
+    // GOOGLE_API_KEY: "AIzaSyCMwJndVwFmklggZ4rOJODwmdqJ2zpiuiM",
+    // GoogleAppID: "83564624583-vj9mffplkq9ltd1heghn0hsuivekltm9.apps.googleusercontent.com",
+    // LinkedInAppID: "78yv35ku1j8yvo",
+    // TwitterAppID: "60o0ohtNXJJQRojKdPjks4kBQ",
+
+    production: false,
+    HOST: 'http://next.learnt.io',
+    WEBCAL: 'webcal://api.next.learnt.io',
+    API_HOST: 'https://api.next.learnt.io',
+    REFERRAL_URL: "https://next.learnt.io",
+    API_SOCKET_URL: 'wss://api.next.learnt.io/ws',
+    COOKIE_DOMAIN: '.next.learnt.io',
+    MAPBOX_TOKEN: 'pk.eyJ1IjoidHV0b3JhcHAiLCJhIjoiY2luNjJrYThqMGU4NHZobTRrb3N5dW8ydyJ9.tWCvx9Qix0jRtOpYN695CQ',
+    FacebookAppID: '434644221081502',
+    GoogleMapsApiKey: '',
+    INTERCOM_APP_ID: 'vj5hlyew',
+    NATIVEFORMS_LICENSE: 'vj5hlyew',
+    SURVEY_DAYS_RECURRENT: 7,
+    GOOGLE_API_KEY: "AIzaSyCMwJndVwFmklggZ4rOJODwmdqJ2zpiuiM",
+    GoogleAppID: "83564624583-vj9mffplkq9ltd1heghn0hsuivekltm9.apps.googleusercontent.com",
+    LinkedInAppID: "78yv35ku1j8yvo",
+    TwitterAppID: "60o0ohtNXJJQRojKdPjks4kBQ",
+};
+
